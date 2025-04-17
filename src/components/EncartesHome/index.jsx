d4b27807063f045cd618ef4e4cd74fed975994df
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-17 11:28:13 -0300

    Create: sessão encartes

--- src/components/EncartesHome/index.jsx
+++ src/components/EncartesHome/index.jsx
@@ -1,21 +1,29 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MascoteEncarte from "../../assets/mascote-encarte.png";
+import CestaDeFrutas from "../../assets/cesta-de-frutas.png";
 import { Container, Row, Col } from "react-bootstrap";
 
 const EncartesHome = () => {
   return (
     <>
         <Container>
-            <Row className="p-5">
-                <Col>
-                    <img src={MascoteEncarte} alt="Mascote Encarte" />
+            <Row className="p-5 d-flex gap-5 justify-content-center">
+                <Col sm={6} md={6} lg={2} className="d-flex justify-content-center">
+                    <img className="mascote-encarte hide-mobile" src={MascoteEncarte} alt="Mascote Encarte" />
                 </Col>
                 
-                <Col className="conteudo-encartes">
+                <Col sm={6} md={6} lg={4} className="conteudo-encartes d-flex justify-content-center">
                     <h3 className="titulo-encarte">Encartes</h3>
                     <h3 className="subtitulo-encartes">Confira as ofertas do Medeiros Supermercado</h3>
-                    <div className="btn-ofertas">OFETAS DA SEMANA</div>
+
+                    <div>
+                        <button className="btn-encarte">OFERTAS DA SEMANA</button>
+                    </div>
+
+                    <div className="hide-mobile">
+                        <img className="cesta-frutas-img" src={CestaDeFrutas} alt="cesta de frutas" />
+                    </div>
                 </Col>
             </Row>
         </Container>
