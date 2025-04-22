f11658809ecf2504ecca961f221de5fd2bfcdf57
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-22 11:26:52 -0300

    props-horario

--- src/components/MyStoreCard/index.jsx
+++ src/components/MyStoreCard/index.jsx
@@ -1,14 +1,14 @@
 
-const MyStoreCard = ({endereco}) => {
+const MyStoreCard = ({endereco, horario}) => {
     return ( <>
             <div className="card-mystore">
                 <div className=".content-card-store">
                     <p><strong>Endereço:</strong> {endereco}</p>
-                    <p><strong>Horario de funcionamento:</strong> Horário de funcionamento todos os dias: 6:00 até as 22:00</p>
+                    <p><strong>Horario de funcionamento:</strong> {horario}</p>
                     <p><strong>Tel:</strong> (85)3291-2233</p>
                     
-                    <button className="btn-card">VER LOJA</button>
                 </div>
+                    <button className="btn-card">VER LOJA</button>
 
             </div>
     </> );
