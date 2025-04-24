79167eb5240689c0ed1f5648a52f3cd198d6e9b2
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-24 10:26:45 -0300

    fix: Route

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import RouterPages from './Routes/index.jsx'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterPages />
   </StrictMode>,
 )
+
