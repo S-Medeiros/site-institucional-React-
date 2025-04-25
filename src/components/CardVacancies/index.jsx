32b25e0857736bda0b591c88f4d32c3c2fa8f7b4
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-25 11:22:24 -0300

    fix: botão de detalhes
    Foi removido o botão, deixando sempre o card aberto.

--- src/components/CardVacancies/index.jsx
+++ src/components/CardVacancies/index.jsx
@@ -1,27 +1,16 @@
-import { useState } from 'react';
-
 const CardVacancies = ({ imagem, titulo, detalhes }) => {
-  const [showDetails, setShowDetails] = useState(false);
-
-  const toggleDetails = () => setShowDetails(!showDetails);
-
   return (
-    <div className={`card-vaga ${showDetails ? 'expanded' : ''}`}>
+    <div className="card-vaga expanded">
       <img src={imagem} alt={titulo} className="imagem-vaga" />
 
       <h5 className="titulo-vaga">{titulo}</h5>
 
-      <button className="botao-detalhes" onClick={toggleDetails}>
-        DETALHES
-      </button>
-
-      {showDetails && (
-        <div className="detalhes-vaga">
-          {detalhes.split('\n').map((linha, i) => (
-            <p key={i}>{linha}</p>
-          ))}
-        </div>
-      )}
+      <div className="detalhes-vaga">
+        <h5 className="text-center bg-danger p-1 border-2">Detalhes da vaga</h5>
+        {detalhes.split('\n').map((linha, i) => (
+          <p key={i}>{linha}</p>
+        ))}
+      </div>
     </div>
   );
 };
