f7f8d77ba5514399b1e168754198f3249f8c2922
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-25 10:20:32 -0300

    Update: novasVagas

--- src/pages/Work/index.jsx
+++ src/pages/Work/index.jsx
@@ -54,44 +54,177 @@ Prazo para envio dos currículos: Sem prazo definido
 
 const PageWork = () => {
     return ( <>
-        <Container className="p-5">
+        <Container className="p-5 d-flex flex-column gap-5">
             <div className="text-dark-green">
                 <h1>Venha fazer parte da família Medeiros!</h1>
                 <h2>Oportunidades abertas:</h2>
             </div>
 
-
-
-            <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+            {/* Vagas Pref. José Walter Av. J */}
+            <div className="">
+              <h3>Vagas Pref. José Walter Av. J :</h3>
+              <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+              
                 <CardVacancies
-                  imagem={Vaga1}
-                  titulo="OPERADOR DE CAIXA"
-                  detalhes={detalhesTexto2}
-                />
-                <CardVacancies
-                  imagem={Vaga1}
-                  titulo="REPOSITOR"
-                  detalhes={detalhesTexto}
-                />
+                    imagem={Vaga1}
+                    titulo="OPERADOR DE CAIXA (ESTÁGIO)"
+                    detalhes={detalhesTexto2}
+                  />
 
-                <CardVacancies
-                  imagem={Vaga1}
-                  titulo="ASSISTENTE FINANCEIRO"
-                  detalhes={detalhesTexto3}
-                />
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="FRENTE DE LOJA"
+                    detalhes={detalhesTexto3}
+                  />
 
-                <CardVacancies
-                  imagem={Vaga1}
-                  titulo="ASSISTENTE FINANCEIRO"
-                  detalhes={detalhesTexto3}
-                />
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="(PCD)"
+                    detalhes={detalhesTexto}
+                  />
 
-                <CardVacancies
-                  imagem={Vaga1}
-                  titulo="ASSISTENTE FINANCEIRO"
-                  detalhes={detalhesTexto3}
-                />
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="ASSISTENTE FINANCEIRO"
+                    detalhes={detalhesTexto3}
+                  />
+                  
+              </div>
             </div>
+
+            {/* Vagas Pref. José Walter Av. I */}
+            <div className=""> 
+              <h3>Vagas Pref. José Walter Av. I :</h3>
+              <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+              
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="AJUDANTE DE ENTREGA"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="REPOSITOR"
+                    detalhes={detalhesTexto}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="BALCONISTA DE FRIOS"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="OPERADOR DE CAIXA (HORISTA)"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="FRENTE DE LOJA"
+                    detalhes={detalhesTexto2}
+                  />
+
+
+              </div>
+            </div>
+
+            {/* Vagas para loja 3 Jereissati-Pacayuba  */}
+            <div>
+            <div className="">
+              <h3>Vagas para loja 3 - Pacatuba:</h3>
+              <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+              
+                <h4>Sem vagas abertas no momento.</h4>
+
+              </div>
+            </div>
+            </div>
+
+          {/* Vagas para lojas 04 Siqueira */} 
+            <div className="">
+              <h3>Vagas para loja 4 - Siqueira:</h3>
+              <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+              
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="OPERADOR DE CAIXA"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="FRENTE DE LOJA"
+                    detalhes={detalhesTexto3}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="REPOSITOR FLV"
+                    detalhes={detalhesTexto}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="ESTAGIARIO"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="APRENDIZ"
+                    detalhes={detalhesTexto2}
+                  />
+
+              </div>
+            </div>
+
+            {/* Vagas para Conj. Palmeiras */}
+            <div className=""> 
+              <h3>Vagas para loja 5 - Conj. Palmeiras :</h3>
+              <div className="d-flex flex-wrap justify-content-center gap-4 mt-5">
+              
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="APRENDIZ"
+                    detalhes={detalhesTexto2}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="OP.CAIXA (ESTÁGIO / INTEGRAL)"
+                    detalhes={detalhesTexto}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="PREVENÇÃO DE PERDAS"
+                    detalhes={detalhesTexto3}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="AUX. DEPOSITO"
+                    detalhes={detalhesTexto3}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="REPOSITOR DE FRIOS"
+                    detalhes={detalhesTexto3}
+                  />
+
+                  <CardVacancies
+                    imagem={Vaga1}
+                    titulo="FRENTE DE LOJA"
+                    detalhes={detalhesTexto3}
+                  />
+              </div>
+            </div>
+
+
         </Container>
     </> );
 }
