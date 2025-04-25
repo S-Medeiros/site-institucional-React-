c44d4150fa9dda62656e7c8a0ecefc9745361d34
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-04-25 19:43:06 -0300

    sytle-borderBtn

--- src/components/ClubDeDesconto/index.jsx
+++ src/components/ClubDeDesconto/index.jsx
@@ -9,9 +9,14 @@ const ClubeDeDesconto = () => {
             <div className="d-flex align-items-center justify-content-center p-5">
                 <div className="d-flex flex-column gap-5">
                     <h1>Cadastre-se <span className="clubeSpan"><br />GRÁTIS</span> para economizar <sapn className="clubeSpan">MAIS!</sapn></h1>
-                    <Link target="_blank" className="d-flex justify-content-center" to="https://cadastramento-medeiros.mercafacil.com/home">
-                            <button className="btn-encarte" formTarget="black_">Cadastre-se agora</button>
+                    <Link
+                      target="_blank"
+                      to="https://cadastramento-medeiros.mercafacil.com/home"
+                      className="btn-encarte d-flex justify-content-center border border-success"
+                    >
+                      Cadastre-se agora
                     </Link>
+                        
                 </div>
 
                 <img src={mascote} alt="mascote" className="w-25" />
