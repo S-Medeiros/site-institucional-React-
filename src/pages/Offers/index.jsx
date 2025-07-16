6b4943a58a70a2d76806d971a84128fed801fe85
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-07-16 17:26:18 -0300

    atualiza-encartes

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,9 +1,5 @@
 import { Container } from "react-bootstrap";
 import EncarteSemanal1 from "../../assets/01.jpeg"
-import EncarteSemanal2 from "../../assets/02.jpeg"
-import EncarteSemanal3 from "../../assets/03.jpeg"
-import EncarteSemanal4 from "../../assets/04.jpeg"
-import EncarteSemanal5 from "../../assets/05.jpeg"
 
 import { Link } from "react-router-dom";
 
@@ -16,10 +12,6 @@ const Offers = () => {
 
             <h1 className="text-dark-green ">Confira os nossos encartes de oferta!</h1>
             <img src={EncarteSemanal1} alt="Encarte1" className="img-fluid "/>
-            <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid "/>
-            <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid "/>
-            <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid "/>
-            <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid "/>
             
 
             {/* <h3 className="text-light bg-primary">Encarte semanal - Ofertas validas de <strong>05 de Maio até o dia 11 de Maio</strong> de 2025</h3> */}
