ae93ee6a988ce4dac5147862c044a06e33a6435e
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-08-11 10:21:44 -0300

    fix-imgEncarte

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,10 +1,9 @@
 import { Container } from "react-bootstrap";
+import EncarteSemanal0 from "../../assets/encartes/00.jpeg";
 import EncarteSemanal1 from "../../assets/encartes/01.jpeg";
 import EncarteSemanal2 from "../../assets/encartes/02.jpeg";
 import EncarteSemanal3 from "../../assets/encartes/03.jpeg";
 import EncarteSemanal4 from "../../assets/encartes/04.jpeg";
-import EncarteSemanal5 from "../../assets/encartes/05.jpeg";
-import EncarteSemanal6 from "../../assets/encartes/06.jpeg";
 import { Link } from "react-router-dom";
 
 
@@ -15,12 +14,11 @@ const Offers = () => {
 
 
             <h1 className="text-dark-green ">Confira os nossos encartes de oferta!</h1>
+            <img src={EncarteSemanal0} alt="Encarte1" className="img-fluid "/>
             <img src={EncarteSemanal1} alt="Encarte1" className="img-fluid "/>
             <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid "/>
             <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid "/>
             <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid "/>
-            <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid "/>
-            <img src={EncarteSemanal6} alt="Encarte6" className="img-fluid "/>
 
             {/* <h3 className="text-light bg-primary">Encarte semanal - Ofertas validas de <strong>05 de Maio até o dia 11 de Maio</strong> de 2025</h3> */}
 
