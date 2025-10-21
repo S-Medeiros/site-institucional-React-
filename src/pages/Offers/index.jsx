e6c8a2201685c3347ec72a8b0728a1f161cff4ae
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-10-21 17:05:05 -0300

    encarte-semanal

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -3,9 +3,6 @@ import EncarteSemanal0 from "../../assets/encartes/00.jpeg";
 import EncarteSemanal1 from "../../assets/encartes/01.jpeg";
 import EncarteSemanal2 from "../../assets/encartes/02.jpeg";
 import EncarteSemanal3 from "../../assets/encartes/03.jpeg";
-import EncarteSemanal4 from "../../assets/encartes/04.jpeg";
-import EncarteSemanal5 from "../../assets/encartes/05.jpeg";
-import EncarteSemanal6 from "../../assets/encartes/06.jpeg";
 
 import { Link } from "react-router-dom";
 
@@ -25,12 +22,7 @@ const Offers = () => {
               <div className="d-flex gap-3 w-50">
                 <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid"/>
                 <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid "/>
-              </div>
-
-              <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid "/>
-              <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid "/>
-              <img src={EncarteSemanal6} alt="Encarte4" className="img-fluid "/>
-            
+              </div>            
 
           <Link 
             target="_blank"
