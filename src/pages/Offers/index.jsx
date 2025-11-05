04509ba7235427e9a1f39f52ae2b91d126dc5656
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-11-05 11:28:54 -0300

    encartes

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,14 +1,12 @@
 import { Container } from "react-bootstrap";
-import EncarteSemanal0 from "../../assets/encartes/1FLV.jpg";
-import EncarteSemanal1 from "../../assets/encartes/2FLV.jpg";
-import EncarteSemanal2 from "../../assets/encartes/3FLV.jpg";
-import EncarteSemanal3 from "../../assets/encartes/4FLV.jpg";
-import EncarteSemanal4 from "../../assets/encartes/01.jpg";
-import EncarteSemanal5 from "../../assets/encartes/02.jpg";
-import EncarteSemanal6 from "../../assets/encartes/03.jpg";
-import EncarteSemanal7 from "../../assets/encartes/04.jpg";
-import EncarteSemanal8 from "../../assets/encartes/05.jpg";
-import EncarteSemanal9 from "../../assets/encartes/06.jpg";
+import EncarteSemanal0 from "../../assets/encartes/00.jpeg";
+import EncarteSemanal1 from "../../assets/encartes/01.jpeg";
+import EncarteSemanal2 from "../../assets/encartes/02.jpeg";
+import EncarteSemanal3 from "../../assets/encartes/03.jpeg";
+import EncarteSemanal4 from "../../assets/encartes/04.jpeg";
+import EncarteSemanal5 from "../../assets/encartes/05.jpeg";
+import EncarteSemanal6 from "../../assets/encartes/06.jpeg";
+import EncarteSemanal7 from "../../assets/encartes/07.jpeg";
 import { Link } from "react-router-dom";
 
 
@@ -28,12 +26,14 @@ const Offers = () => {
                 <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid"/>
                 <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid "/>
               </div>
-              <img src={EncarteSemanal4} alt="Encarte3" className="img-fluid "/>
-              <img src={EncarteSemanal5} alt="Encarte3" className="img-fluid "/>
-              <img src={EncarteSemanal6} alt="Encarte3" className="img-fluid "/>
-              <img src={EncarteSemanal7} alt="Encarte3" className="img-fluid "/>
-              <img src={EncarteSemanal8} alt="Encarte3" className="img-fluid "/>
-              <img src={EncarteSemanal9} alt="Encarte3" className="img-fluid "/>
+              <div className="d-flex gap-3 w-50">
+                <img src={EncarteSemanal4} alt="Encarte3" className="img-fluid "/>
+                <img src={EncarteSemanal5} alt="Encarte3" className="img-fluid "/>
+              </div>
+              <div className="d-flex gap-3 w-50">
+                <img src={EncarteSemanal6} alt="Encarte3" className="img-fluid "/>
+                <img src={EncarteSemanal7} alt="Encarte3" className="img-fluid "/>
+              </div>
 
           <Link 
             target="_blank"
