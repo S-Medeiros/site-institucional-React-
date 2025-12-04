c76a737ce3db78dde072c80156b4dd4d787f1aef
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-12-04 09:32:42 -0300

    encartes

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap";
-import EncarteSemanal1 from "../../assets/encartes/01.jpeg";
-import EncarteSemanal2 from "../../assets/encartes/02.jpeg";
-import EncarteSemanal3 from "../../assets/encartes/03.jpeg";
-import EncarteSemanal4 from "../../assets/encartes/04.jpeg";
-import EncarteSemanal5 from "../../assets/encartes/05.jpeg";
-/*import EncarteSemanal7 from "../../assets/encartes/07.jpeg";
-import EncarteSemanal8 from "../../assets/encartes/08.jpeg";
-import EncarteSemanal9 from "../../assets/encartes/09.jpeg";
-import EncarteSemanal10 from "../../assets/encartes/010.jpeg";*/
+import EncarteSemanal1 from "../../assets/encartes/01.jpg";
+import EncarteSemanal2 from "../../assets/encartes/02.jpg";
+import EncarteSemanal3 from "../../assets/encartes/03.jpg";
+import EncarteSemanal4 from "../../assets/encartes/04.jpg";
+import EncarteSemanal5 from "../../assets/encartes/05.jpg";
+import EncarteSemanal6 from "../../assets/encartes/1CARNE.png";
+import EncarteSemanal7 from "../../assets/encartes/2CARNE.png";
+import EncarteSemanal8 from "../../assets/encartes/3CARNE.png";
+import EncarteSemanal9 from "../../assets/encartes/4CARNE.png";
+import EncarteSemanal10 from "../../assets/encartes/5CARNE.png";
 import { Link } from "react-router-dom";
 
 
@@ -15,21 +16,23 @@ const Offers = () => {
     return ( <>
     <Container className="p-5">
         <div className="d-flex flex-column gap-5" >
-
-
-            <h1 className="text-dark-green ">Confira os nossos encartes de oferta!</h1>
+            <h1 className="text-dark-green">Confira os nossos encartes de oferta!</h1>
               <img src={EncarteSemanal1} alt="Encarte1" className="img-fluid"/>
               <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid"/>
               <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid"/>
               <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid"/>
               <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid"/>
-
+              <img src={EncarteSemanal6} alt="Encarte5" className="img-fluid"/>
+              <img src={EncarteSemanal7} alt="Encarte5" className="img-fluid"/>
+              <img src={EncarteSemanal8} alt="Encarte5" className="img-fluid"/>
+              <img src={EncarteSemanal9} alt="Encarte5" className="img-fluid"/>
+              <img src={EncarteSemanal10} alt="Encarte5" className="img-fluid"/>
 
           <Link 
             target="_blank"
             to="https://drive.google.com/drive/folders/1AlLyojjIG41AMrJc-_-aCtztK5tEKoYZ"
             className="d-flex justify-content-center btn-encarte"
-          >
+            >
             BAIXE NOSSO ENCARTE
           </Link>
         </div>
