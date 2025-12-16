d8d45dc31896ea9516c8e0d42708e14f2f3a168d
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2025-12-16 15:23:02 -0300

    link slides

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,15 +1,25 @@
 import React, { useState, useEffect } from "react";
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import slider1 from "../../assets/slider0.jpeg";
+
 import slider0 from "../../assets/slider1.png";
+import slider1 from "../../assets/slider0.jpeg";
 import slider2 from "../../assets/slider2.png";
 
 const ImageCarousel = () => {
-  const images = [
-    slider2,
-    slider1,
-    slider0,
+  const slides = [
+    {
+      image: slider2,
+      link: "https://linktr.ee/medeirossupermercados?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAZXh0bgNhZW0CMTEAc3J0YwZhcHBfaWQMMjU2MjgxMDQwNTU4AAGnA0y05HuWtUcWYgaJ6GDCbQGWdIam3OfnMXll40wcm67J2xryJ-KhotUYn30_aem_o7b4WKz3FbRzrQeqlW7EhA",
+    },
+    {
+      image: slider1,
+      link: "https://SEU_LINK_AQUI",
+    },
+    {
+      image: slider0,
+      link: "https://cadastramento-medeiros.mercafacil.com/home",
+    },
   ];
 
   const [index, setIndex] = useState(0);
@@ -21,22 +31,28 @@ const ImageCarousel = () => {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setIndex((prevIndex) => (prevIndex + 1) % slides.length);
     }, intervalTime);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [slides.length]);
 
   return (
     <Carousel activeIndex={index} onSelect={handleSelect} interval={null}>
-      {images.map((image, idx) => (
-        <Carousel.Item className="color-danger" key={idx}>
-          <img
-            className="d-block w-100"
-            src={image}
-            alt={`Slide ${idx + 1}`}
-            style={{ height: "auto", objectFit: "cover" }}
-          />
+      {slides.map((slide, idx) => (
+        <Carousel.Item key={idx}>
+          <a
+            href={slide.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              className="d-block w-100"
+              src={slide.image}
+              alt={`Slide ${idx + 1}`}
+              style={{ cursor: "pointer", objectFit: "cover" }}
+            />
+          </a>
         </Carousel.Item>
       ))}
     </Carousel>
