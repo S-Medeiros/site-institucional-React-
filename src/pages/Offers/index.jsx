fac1cf6f8d5f953042bcbd617848ed7e8191d5c2
Francisco José <133537749+franciscojose96@users.noreply.github.com>
2026-01-07 11:31:11 -0300

    encartes update

--- src/pages/Offers/index.jsx
+++ src/pages/Offers/index.jsx
@@ -1,15 +1,13 @@
 import { Container } from "react-bootstrap";
-import EncarteSemanal0 from "../../assets/encartes/00.jpeg";
-import EncarteSemanal1 from "../../assets/encartes/01.jpeg";
-import EncarteSemanal2 from "../../assets/encartes/02.jpeg";
-import EncarteSemanal3 from "../../assets/encartes/03.jpeg";
-import EncarteSemanal4 from "../../assets/encartes/04.jpeg";
-import EncarteSemanal5 from"../../assets/encartes/05.jpeg";
-// import EncarteSemanal5 from "../../assets/encartes/CARNE01.jpeg";
-// import EncarteSemanal6 from "../../assets/encartes/CARNE02.jpeg";
-// import EncarteSemanal7 from "../../assets/encartes/CARNE03.jpeg";
-// import EncarteSemanal8 from "../../assets/encartes/CARNE04.jpeg";
-// import EncarteSemanal9 from "../../assets/encartes/CARNE05.jpeg";
+import EncarteSemanal0 from "../../assets/encartes/00-carnes.jpeg";
+import EncarteSemanal1 from "../../assets/encartes/01-carnes.jpeg";
+import EncarteSemanal2 from "../../assets/encartes/02-carnes.jpeg";
+import EncarteSemanal3 from "../../assets/encartes/03-carnes.jpeg";
+import EncarteSemanal4 from "../../assets/encartes/04-carnes.jpeg";
+import EncarteSemanal5 from "../../assets/encartes/00-flv.jpeg";
+import EncarteSemanal6 from "../../assets/encartes/01-flv.jpeg";
+import EncarteSemanal7 from "../../assets/encartes/02-flv.jpeg";
+import EncarteSemanal8 from "../../assets/encartes/03-flv.jpeg";
 import { Link } from "react-router-dom";
 
 
@@ -19,16 +17,23 @@ const Offers = () => {
         <div className="d-flex flex-column gap-5" >
             <h1 className="text-dark-green">Confira os nossos encartes de oferta!</h1>
               <div className="d-flex w-50 gap-3">
+                <img src={EncarteSemanal0} alt="Encarte0" className="img-fluid"/>
                 <img src={EncarteSemanal1} alt="Encarte1" className="img-fluid"/>
-                <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid"/>
               </div>
               <div className="d-flex w-50 gap-3">
+                <img src={EncarteSemanal2} alt="Encarte2" className="img-fluid"/>
                 <img src={EncarteSemanal3} alt="Encarte3" className="img-fluid"/>
-                <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid"/>
               </div>
+              <div className="d-flex w-50 gap-3">
+                <img src={EncarteSemanal4} alt="Encarte4" className="img-fluid"/>
+                <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid"/>
+              </div>
+              <div className="d-flex w-50 gap-3">
+                <img src={EncarteSemanal6} alt="Encarte6" className="img-fluid"/>
+                <img src={EncarteSemanal7} alt="Encarte7" className="img-fluid"/>
+              </div>
+              <img src={EncarteSemanal8} alt="Encarte8" className="img-fluid"/>
 
-              <img src={EncarteSemanal5} alt="Encarte5" className="img-fluid"/>
-  
           <Link 
             target="_blank"
             to="https://drive.google.com/drive/folders/1AlLyojjIG41AMrJc-_-aCtztK5tEKoYZ"
